9b30ca7425488bef76cc9972483f2cab28aa3e58
Daniel Jin <danieljin.47@gmail.com>
2020-09-15 17:38:06 +0000

    access data using covid19api API

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ import pandas as pd
 from folium.plugins import HeatMap
 from pandas.io.json import json_normalize
 
+conn=http.client.HTTPSConnection("api.covid19api.com")
+payload=''
+headers={}
+conn.request("GET","/summary",payload,headers)
+res=conn.getresponse()
+data=res.read().decode('UTF-8')
